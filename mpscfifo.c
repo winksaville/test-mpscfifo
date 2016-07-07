35701e45b9d3f31baa18dc2e0277a6c6374e71c3
Wink Saville <wink@saville.com>
2016-07-07 15:21:11 -0700

    Add DPF to mpscfifo.c

--- mpscfifo.c
+++ mpscfifo.c
@@ -40,6 +40,8 @@
  * @see mpscfifo.h
  */
 MpscFifo_t *initMpscFifo(MpscFifo_t *pQ, Msg_t *pStub) {
+  DPF("initMpscFifo: pQ=%p stub=%p stub->pPool=%p\n",
+      pQ, pStub, pStub->pPool);
   pStub->pNext = NULL;
   pQ->pHead = pStub;
   pQ->pTail = pStub;
@@ -55,16 +57,17 @@ Msg_t *deinitMpscFifo(MpscFifo_t *pQ) {
   pQ->pTail = NULL;
   if (pStub->pPool == NULL) {
     // Return stub as its doesn't blelow to a pool
-    DPF("deinitMpscFifo: pQ=%p no pool for stub=%p\n", pQ, pStub);
+    DPF("deinitMpscFifo:-pQ=%p no pool for stub=%p\n", pQ, pStub);
     return pStub;
   } else if (pStub->pPool == pQ) {
     // Can't return the stub to the poll we're deinitializing
-    DPF("deinitMpscFifo: pQ=%p don't ret our own stub=%p\n", pQ, pStub);
+    DPF("deinitMpscFifo:-pQ=%p don't ret our own stub=%p\n", pQ, pStub);
     return NULL;
   } else {
     // Return the stub to the pool
-    DPF("deinitMpscFifo: pQ=%p ret stub=%p\n", pQ, pStub);
+    DPF("deinitMpscFifo:+pQ=%p ret stub=%p stub->pPool=%p\n", pQ, pStub, pStub->pPool);
     ret(pStub);
+    DPF("deinitMpscFifo:-pQ=%p ret stub=%p stub->pPool=%p\n", pQ, pStub, pStub->pPool);
     return NULL;
   }
 }
@@ -73,11 +76,15 @@ Msg_t *deinitMpscFifo(MpscFifo_t *pQ) {
  * @see mpscifo.h
  */
 void add(MpscFifo_t *pQ, Msg_t *pMsg) {
+  DPF("add:+pQ=%p msg=%p arg1=%lu arg2=%lu\n",
+        pQ, pMsg, pMsg->arg1, pMsg->arg2);
   pMsg->pNext = NULL;
   void** ptr_pHead = (void*)&pQ->pHead;
   Msg_t* pPrev = __atomic_exchange_n(ptr_pHead, pMsg, __ATOMIC_SEQ_CST);
   // rmv will stall spinning if preemted at this critical spot
   pPrev->pNext = pMsg;
+  DPF("add:-pQ=%p msg=%p arg1=%lu arg2=%lu\n",
+        pQ, pMsg, pMsg->arg1, pMsg->arg2);
 }
 
 /**
@@ -91,8 +98,11 @@ Msg_t *rmv_non_stalling(MpscFifo_t *pQ) {
     pTail->arg1 = pNext->arg1;
     pTail->arg2 = pNext->arg2;
     pQ->pTail = pNext;
+    DPF("rmv_non_stailling: got msg pQ=%p msg=%p arg1=%lu arg2=%lu\n",
+        pQ, pTail, pTail->arg1, pTail->arg2);
     return pTail;
   } else {
+    DPF("rmv_non_stalling: 'empty' pQ=%p msg=NULL\n", pQ);
     return NULL;
   }
 }
@@ -105,20 +115,25 @@ Msg_t *rmv(MpscFifo_t *pQ) {
   Msg_t* pNext = pTail->pNext;
   if ((pNext == NULL) && (pTail == pQ->pHead)) {
     // Q is empty
+    DPF("rmv: empty pQ=%p pNext=%p pTail=%p == pHead=%p\n",
+        pQ, pNext, pTail, pQ->pHead);
     return NULL;
   } else {
     if (pNext == NULL) {
       // Q is NOT empty but producer was preempted at the critical spot
+      DPF("rmv: stalling pQ=%p pNext=%p pTail=%p != pHead=%p\n",
+          pQ, pNext, pTail, pQ->pHead);
       uint32_t i;
       for (i = 0; (pNext = pTail->pNext) == NULL; i++) {
         sched_yield();
       }
-      DPF("rmv: Bad luck producer was prempted pQ=%p i=%d\n", pQ, i);
     }
     pTail->pRspQ = pNext->pRspQ;
     pTail->arg1 = pNext->arg1;
     pTail->arg2 = pNext->arg2;
     pQ->pTail = pNext;
+    DPF("rmv: got msg pQ=%p msg=%p arg1=%lu arg2=%lu\n",
+        pQ, pTail, pTail->arg1, pTail->arg2);
     return pTail;
   }
 }
@@ -128,7 +143,16 @@ Msg_t *rmv(MpscFifo_t *pQ) {
  */
 void ret(Msg_t* pMsg) {
   if ((pMsg != NULL) && (pMsg->pPool != NULL)) {
+    DPF("ret: pool=%p msg=%p arg1=%lu arg2=%lu\n",
+        pMsg->pPool, pMsg, pMsg->arg1, pMsg->arg2);
     add(pMsg->pPool, pMsg);
+  } else {
+    if (pMsg == NULL) {
+      DPF("ret: No msg msg=%p\n", pMsg);
+    } else {
+      DPF("ret: No pool pool=%p msg=%p arg1=%lu arg2=%lu\n",
+          pMsg->pPool, pMsg, pMsg->arg1, pMsg->arg2);
+    }
   }
 }
 
@@ -140,8 +164,12 @@ void send_rsp_or_ret(Msg_t* msg, uint64_t arg1) {
     MpscFifo_t* pRspQ = msg->pRspQ;
     msg->pRspQ = NULL;
     msg->arg1 = arg1;
+    DPF("send_rsp_or_ret: send pRspQ=%p rsp arg1=%lu arg2=%lu\n",
+        pRspQ, msg->arg1, msg->arg2);
     add(pRspQ, msg);
   } else {
+    DPF("send_rsp_or_ret: ret msg=%p arg1=%lu arg2=%lu\n",
+        msg, msg->arg1, msg->arg2);
     ret(msg);
   }
 }
